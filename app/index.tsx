7154cdcfd0647a65e35d59c017eccab380d4256e
NA <na@Naims-MacBook-Pro.local>
2024-12-27 18:19:58 +0000

    eslint prettier

---
+++ app/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Text, View } from "react-native";
+
+const Index = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+};
+
+Index.propTypes = {};
+
+export default Index;
